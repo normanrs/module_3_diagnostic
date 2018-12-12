c47806ed73bd4c2fc59240ab5acf9ff00fafa2e8
normanrs <normanrs@live.com>
2018-12-12 11:05:22 -0700

    cannot get faraday request to work but have a working get request in postman

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -5,20 +5,22 @@ class NrelService
 	end
 
   def get_stations
-    get_json("/user/repos")
+    get_json("/api/alt-fuel-stations/v1/nearest")
   end
 
   def get_json(url)
-		response = conn.get(url)
+    conn = Faraday.new
+    response = conn.get do |req|
+      req.url "https://developer.nrel.gov#{url}"
+      req.headers( ["fuel_type"] = "ELEC, LPG",
+                    limit: 10,
+                    location: @zip,
+                    radius: 6,
+                    api_key: ENV["nrel_key"],
+                  }
+    end
+    require "pry"; binding.pry
 		parsed = JSON.parse(response.body, symbolize_names: true)
-	end
-
-  def conn
-		Faraday.new(url: "https://api.github.com") do |faraday|
-			faraday.headers["Authorization"] = "token #{@user.github_key}"
-
-			faraday.adapter Faraday.default_adapter
-		end
 	end
 
 end
