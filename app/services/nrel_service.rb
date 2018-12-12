ae84b870e4f408d9a1ae7d48f217c492fa81f465
normanrs <normanrs@live.com>
2018-12-12 11:11:25 -0700

    removed pry

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -5,21 +5,10 @@ class NrelService
 	end
 
   def get_stations
-    get_json("/api/alt-fuel-stations/v1/nearest")
-  end
-
-  def get_json(url)
     conn = Faraday.new
     response = conn.get do |req|
-      req.url "https://developer.nrel.gov#{url}"
-      req.headers( ["fuel_type"] = "ELEC, LPG",
-                    limit: 10,
-                    location: @zip,
-                    radius: 6,
-                    api_key: ENV["nrel_key"],
-                  }
+      req.url "# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=ELEC, LPG&limit=10&api_key=#{ENV["nrel_key"]}&location=#{@zip}&radius=6"
     end
-    require "pry"; binding.pry
 		parsed = JSON.parse(response.body, symbolize_names: true)
 	end
 
