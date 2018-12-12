c361794918c588cd2669bd740fd6c7a9233302ba
normanrs <normanrs@live.com>
2018-12-12 09:19:03 -0700

    wrote first test for basic search form and path

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
 feature "User can visit root page" do
   scenario "and view navbar contents" do
     visit "/"
@@ -7,5 +16,13 @@ feature "User can visit root page" do
       expect(page).to have_content("AltFuelFinder")
       expect(page).to have_selector("input[value='Search by zip...']")
     end
+  end
+
+  it "and click on locate to be taken to a search page" do
+    visit "/"
+    fill_in('Search', :with => 80203)
+    click_on "Locate"
+
+    expect(current_path).to eq(search_path)
   end
 end
